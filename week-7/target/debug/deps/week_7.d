0b23562d5a32f38a99505fc6b93011f19d267288
Alamutu Daniel <daniel.alamutu@pau.edu.ng>
2025-11-12 18:04:31 +0100

    Function with Parameters

---
+++ week-7/target/debug/deps/week_7.d
@@ -0,0 +1,5 @@
+C:\Users\HP\Documents\d.alamutuCSC101\week-7\target\debug\deps\week_7.d: src\main.rs
+
+C:\Users\HP\Documents\d.alamutuCSC101\week-7\target\debug\deps\week_7.exe: src\main.rs
+
+src\main.rs:
---
+++ week-7/target/debug/week-7.d
@@ -0,0 +1,1 @@
+C:\Users\HP\Documents\d.alamutuCSC101\week-7\target\debug\week-7.exe: C:\Users\HP\Documents\d.alamutuCSC101\week-7\src\main.rs
