d5bcaa68d27e83ca409afbfd76fa4fee42be2b89
Alamutu Daniel <daniel.alamutu@pau.edu.ng>
2025-11-19 17:00:37 +0100

    (Creating a vector in Rust)

--- week-8/practice_1/src/main.rs
+++ week-8/practice_1/src/main.rs
@@ -1,3 +1,15 @@
 fn main() {
-    println!("Hello, world!");
+
+    // Using Vec::new()
+    let v : Vec<i64> = Vec::new();
+
+    // printing the size of vector
+    println!("\nThe length of Vec::new is: {}",v.len());
+
+    // Using macro
+    let v = vec!["Grace","Effiong","Basil","Kareem","Susan"];
+
+    //printin the size of vector
+    println!("\nThe length of vec macro is: {}",v.len());
+
 }
